6bfdcabfa0969352de99fcbebc061b4ae259a693
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-14 13:35:56 +0200

    harjoituksia

---
+++ harjoitus3.py
@@ -0,0 +1,24 @@
+from pprint import pformat, pprint
+from time import ctime
+import time
+
+aikaleima = time.time()
+
+esimerkki_kuvio = {
+    'nimi': 'Esimerkki',
+    'tyyppi': 'monikulmio',
+    'luotu': ctime(aikaleima),
+    'pisteet': [
+        {'x': 10, 'y': 20, 'z': 30},
+        {'x': 20, 'y': 5, 'z': 10},
+        {'x': 30, 'y': 15, 'z': 20},
+    ]
+}
+
+pprint(esimerkki_kuvio, sort_dicts=False)
+
+esimerkki_kuvio_muotoilu_teksti = pformat (esimerkki_kuvio)
+
+# Muutetaan muotoilu teksti isoiksi kirjaimiksi ja tulostetaan
+# Huom: \n = rivinvaihto
+print('Muotoiltu teksti:\n', esimerkki_kuvio_muotoilu_teksti.upper())
